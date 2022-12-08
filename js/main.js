b77e47d544cdf453b214753f365bcb99b75f5c7d
jin <jinbyun712@gmail.com>
2022-12-08 15:33:44 +0900

    중간저장

--- js/main.js
+++ js/main.js
@@ -11,6 +11,7 @@ const swiper = new Swiper('.swiper', {
   // Optional parameters
   direction: 'horizontal',
   loop: true,
+  debugger: true,
   // If we need pagination
   pagination: {
     el: '.swiper-pagination',
@@ -22,9 +23,33 @@ const swiper = new Swiper('.swiper', {
     prevEl: '.swiper-button-prev',
   },
 
-  slidesPerView: 1,
-  spaceBetween: 30,
+  slidesPerView:1.4,
+  spaceBetween: 25,
   centeredSlides: true,
   autoplay: false
 
 });
+
+
+
+
+
+///*bottom_menu */ 
+//width-recognition
+$(window).resize(function(){
+  const windowWidth = $(window).width();
+  if (windowWidth >= 800) {
+    $(".bottom_menu_wrap").slideUp(0);
+  } else{
+    $(".bottom_menu_wrap").slideDown(0);
+  }
+});
+//height-recognition
+$(window).scroll(function(){
+  const scrollHeight = $(window).scrollTop();
+  if (scrollHeight > 80) {
+    $(".bottom_menu_wrap").slideDown(0);
+  } else {
+    $(".bottom_menu_wrap").slideUp(0);
+  }
+})
