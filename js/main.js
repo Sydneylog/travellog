f0ca77d0210b81cb248c5824bdd1f3b83f8f1247
jin <jinbyun712@gmail.com>
2022-12-09 15:38:22 +0900

    responsiveness for tablet & PWA javascript

--- js/main.js
+++ js/main.js
@@ -1,23 +1,35 @@
-// enroll service worker
-// if ('serviceWorker' in navigator){
-//   navigator.serviceWorker
-//   .register('./js/service_worker.js')
-//   .then(function(){
-//     console.log('서비스 워커가 등록됨');
-//   })
-// }
+/**service_worker**/
 
 
 
+/**mobile_menu**/
+/*mobile_toggle*/
+$(".mobile_list").click(function(){
+  $(".mobile_menu").fadeToggle()
+});
 
-
+/*arcordian_menu*/
+$(".mobile_menu_list").click(function(){
+  $(this).children('ul').slideToggle();
+});
+/*width-recognition*/
+$(window).resize(function(){
+  const windowWidth = $(window).width();
+  if (windowWidth >= 768) {
+    $(".mobile_menu legend").addClass("blind");
+    $(".mobile_menu").show();
+  } 
+  if (windowWidth < 768) {
+        $(".mobile_menu legend").removeClass("blind");
+  }
+});
 
 
 /** bottom_menu **/ 
 /* width-recognition */
 $(window).resize(function(){
   const windowWidth = $(window).width();
-  if (windowWidth >= 800) {
+  if (windowWidth >= 768) {
     $(".bottom_menu_wrap").slideUp(0);
   } else{
     $(".bottom_menu_wrap").slideDown(0);
