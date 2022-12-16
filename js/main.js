26ce4e8e97c08fb021c74c4e38c0585efc3f8bbc
jin <jinbyun712@gmail.com>
2022-12-16 18:24:05 +0900

    findout present location & chaining other APIs

--- js/main.js
+++ js/main.js
@@ -3,7 +3,104 @@ const imageToggleChecked= document.getElementById("imageToggleCheck");
 
 
 /*** window.onload lightness mod setting & API call ***/
-  window.onload = function(){
+  window.onload = () => {
+  /** geolocation **/
+  navigator.geolocation.getCurrentPosition(function locationNow (position) {
+    //console.log(position);
+    const latitude = position.coords.latitude;
+    const longitude = position.coords.longitude;
+    let nowDate = new Date().toISOString().substring(0,10).replace(/-/g,'');
+    let currencyHere = String(geoplugin_currencyCode()); 
+    console.log(currencyHere);
+
+    /* saving OPEN API url */
+    //weather API url
+    const weatherUrl = `https://api.openweathermap.org/data/2.5/weather?lat=${latitude}&lon=${longitude}&appid=fca7b6ecde13fa2d4e140006f768fd79&units=metric`;
+    //exchange rate API url
+    const exchageUrl = `https://quotation-api-cdn.dunamu.com/v1/forex/recent?codes=FRX.KRW${currencyHere}`;
+    //find out currencyUnit
+    const currencyUnit = `https://www.koreaexim.go.kr/site/program/financial/exchangeJSON?authkey=HwAgZvweofSqx5Uqjzp2IgWOTrVQBo24$&searchdate=${nowDate}&data=AP01`;
+
+   /* API-weather */
+    fetch(weatherUrl)
+      .then((res) => res.json())
+      .then((myJson) => {
+        console.log(myJson);
+        const weatherIcon = document.getElementById("weatherIcon");
+        const weatherIconId = myJson.weather[0].icon;
+        const weatherCountry = myJson.sys.country;
+
+        document.getElementById("wCountry").innerText = myJson.sys.country;
+        document.getElementById("wCity").innerText = myJson.name;
+        document.getElementById("weather").innerText = myJson.weather[0].main;
+        document.getElementById("temperature").innerText = myJson.main.temp;
+        document.getElementById("humidity").innerText = myJson.main.humidity;
+
+        weatherIcon.setAttribute("src", "http://openweathermap.org/img/wn/" + weatherIconId + "@2x.png");
+
+        //diplomat notice API
+        const diplomatSafteyUrl = `https://apis.data.go.kr/1262000/CountrySafetyService3/getCountrySafetyList3?serviceKey=RHh9qBtKX0nX7AHYN9wc37tOXdekXhwz8L07fm3vc3rReNkBkkWM6YUaB0Eo3YDEiN7rRKN4mTfwePoyCFFUZA%3D%3D&returnType=JSON&numOfRows=2&cond[country_iso_alp2::EQ]=${weatherCountry}&pageNo=1`;
+
+
+        //diplomat flag API 
+        const flagUrl = `https://apis.data.go.kr/1262000/CountryFlagService2/getCountryFlagList2?serviceKey=RHh9qBtKX0nX7AHYN9wc37tOXdekXhwz8L07fm3vc3rReNkBkkWM6YUaB0Eo3YDEiN7rRKN4mTfwePoyCFFUZA%3D%3D&returnType=JSON&numOfRows=1&cond[country_iso_alp2::EQ]=${weatherCountry}&pageNo=1`
+
+      
+        /* diplomat saftey notice */
+        fetch(diplomatSafteyUrl)
+        .then((res) => res.json())
+        .then((myJson) => {
+        //console.log(myJson);
+        document.getElementById("noticeCountry").innerText = myJson.data[0].country_nm
+        ;
+          for(i = 0; i < myJson.data.length; i++){
+            //console.log(myJson.data[String(i)]);
+            document.getElementById("dSafetyTitle" + i).innerText = myJson.data[i].title;
+            document.getElementById("warning" + i).innerText = myJson.data[i].ctgy_nm;
+            document.getElementById("dSafetyDate" + i).innerText = myJson.data[i].wrt_dt;
+            if(myJson.data[i].ctgy_nm == '주의'){
+              document.getElementById("warning" + i).classList.add('red_btn')
+            } else if (myJson.data[i].ctgy_nm == '안내'){
+              document.getElementById("warning" + i).classList.add('green_btn')
+            } else {
+              document.getElementById("warning" + i).classList.add('')
+            }
+          }
+        });
+        /* flag */
+        fetch(flagUrl)
+        .then((res) => res.json())
+        .then((myJson) => {
+          console.log(myJson);
+          const downUrl = myJson.data[0].download_url;
+          const flagIcon = document.getElementById("flagIcon");
+
+          flagIcon.setAttribute("src", `${downUrl}`);
+        });
+
+
+
+
+      });
+
+        /* exchange rate */
+        fetch(exchageUrl)
+        .then((res) => res.json())
+        .then((myJson) => {
+        //console.log(myJson[0]);
+        const fxData = myJson[0];
+        const fxUnit = fxData.currencyCode;
+        
+        document.getElementById("fxDate").innerText = fxData.date;
+        document.getElementById("baseRate").innerText = `${fxData.basePrice.toString()} ${fxUnit}`;
+        document.getElementById("fxBuying").innerText = `${fxData.cashBuyingPrice.toString()} ${fxUnit}`;
+    });
+
+    
+
+  });
+  
+
   /** loading lightness mode **/
   const darkIcon = document.querySelector('.dark_icon');
   const lightIcon = document.querySelector('.light_icon');
@@ -24,17 +121,6 @@ const imageToggleChecked= document.getElementById("imageToggleCheck");
   //const presentTheme = () => (isUserColorTheme ? isUserColorTheme : isOsColorTheme);
     console.log('작동되어야 하는 값' + presentTheme)
   //checklist
-  
-
-  /* saving API url */
-    //weather API
-    const weatherUrl = 'https://api.openweathermap.org/data/2.5/weather?lat=13.736717&lon=100.523186&appid=fca7b6ecde13fa2d4e140006f768fd79&units=metric';
-    //exchange rate API
-    const exchageUrl = 'https://quotation-api-cdn.dunamu.com/v1/forex/recent?codes=FRX.KRWTHB';
-    //diplomat notice API
-    const diplomatSafteyUrl = 'https://apis.data.go.kr/1262000/CountrySafetyService3/getCountrySafetyList3?serviceKey=RHh9qBtKX0nX7AHYN9wc37tOXdekXhwz8L07fm3vc3rReNkBkkWM6YUaB0Eo3YDEiN7rRKN4mTfwePoyCFFUZA%3D%3D&returnType=JSON&numOfRows=2&cond[country_iso_alp2::EQ]=TH&pageNo=1';
-    //diplomat flag API
-    const flagUrl = 'https://apis.data.go.kr/1262000/CountryFlagService2/getCountryFlagList2?serviceKey=RHh9qBtKX0nX7AHYN9wc37tOXdekXhwz8L07fm3vc3rReNkBkkWM6YUaB0Eo3YDEiN7rRKN4mTfwePoyCFFUZA%3D%3D&returnType=JSON&numOfRows=1&cond[country_iso_alp2::EQ]=TH&pageNo=1'
 
   /** initializing lightness mode **/
     // console.log(isUserColorTheme);
@@ -79,65 +165,8 @@ const imageToggleChecked= document.getElementById("imageToggleCheck");
   });
     
 
-  /** APIs**/
-    /* weather */
-    fetch(weatherUrl)
-      .then((res) => res.json())
-      .then((myJson) => {
-        //console.log(myJson);
-        const weatherIcon = document.getElementById("weatherIcon");
-        const weatherIconId = myJson.weather[0].icon;
-
-        document.getElementById("wCountry").innerText = myJson.sys.country;
-        document.getElementById("wCity").innerText = myJson.name;
-        document.getElementById("weather").innerText = myJson.weather[0].main;
-        document.getElementById("temperature").innerText = myJson.main.temp;
-        document.getElementById("humidity").innerText = myJson.main.humidity;
-
-        weatherIcon.setAttribute("src", "http://openweathermap.org/img/wn/" + weatherIconId + "@2x.png");
-      });
-    /* exchange rate */
-    fetch(exchageUrl)
-      .then((res) => res.json())
-      .then((myJson) => {
-        //console.log(myJson[0]);
-        const fxData = myJson[0];
-        const fxUnit = fxData.currencyCode;
-        
-        document.getElementById("fxDate").innerText = fxData.date;
-        document.getElementById("baseRate").innerText = `${fxData.basePrice.toString()} ${fxUnit}`;
-        document.getElementById("fxBuying").innerText = `${fxData.cashBuyingPrice.toString()} ${fxUnit}`;
-      });
-    /* diplomat saftey notice */
-    fetch(diplomatSafteyUrl)
-      .then((res) => res.json())
-      .then((myJson) => {
-        //console.log(myJson);
-        document.getElementById("noticeCountry").innerText = myJson.data[0].country_nm
-        ;
-        for(i = 0; i < myJson.data.length; i++){
-          //console.log(myJson.data[String(i)]);
-          document.getElementById("dSafetyTitle" + i).innerText = myJson.data[i].title;
-          document.getElementById("warning" + i).innerText = myJson.data[i].ctgy_nm;
-          document.getElementById("dSafetyDate" + i).innerText = myJson.data[i].wrt_dt;
-          if(myJson.data[i].ctgy_nm == '주의'){
-            document.getElementById("warning" + i).classList.add('red_btn')
-          } else if (myJson.data[i].ctgy_nm == '안내'){
-            document.getElementById("warning" + i).classList.add('green_btn')
-          } else {
-            document.getElementById("warning" + i).classList.add('')
-          }
-        }
-      });
-      /* flag */
-      fetch(flagUrl)
-      .then((res) => res.json())
-      .then((myJson) => {
-        console.log(myJson);
-        const flagIcon = document.getElementById("flagIcon");
-
-        flagIcon.setAttribute("src", "https://opendata.mofa.go.kr:8444/fileDownload/images/country_images/flags/214/20220318_170222070.gif");
-      });
+  
+   
   }
 
 
@@ -164,17 +193,32 @@ const imageToggleChecked= document.getElementById("imageToggleCheck");
     }
   });
 /* image_loading_toggle */
-
   imageToggleChecked.addEventListener('click', () => {
   let imagesAll = document.querySelectorAll('img');
+  let refreshHelper = new Date().getTime(); 
     console.log(imagesAll);
     if(imageToggleChecked.checked){
+      // for(i = 0; i < imagesAll.length; i++){
+      //   console.log(imagesAll[i].getAttribute('src'));
+      //   if(imagesAll[i].getAttribute('src') == '../images/baseline_search_black_48dp.png'){
+      //       imagesAll[i].src += '?time=' + refreshHelper;
+      //   }
+      //   console.log('경로 바뀌었나:', imagesAll[i].getAttribute('src'))  
+      // }
+      // localStorage.setItem('imageState', imageToggleChecked.checked = true);
       window.location.reload();
+      
+
     } else {
+
       for(i = 0; i < imagesAll.length; i++){
+
         imagesAll[i].setAttribute('src', '../images/baseline_search_black_48dp.png');
       }
+      localStorage.setItem('imageState', imageToggleChecked.checked = false);
     }
+
+    console.log('이미지토글상태:', imageToggleChecked.checked)
   })
   
   
---
+++ serviceWorker.js
@@ -0,0 +1,39 @@
+if('serviceWorker' in navigator){
+  try {
+    navigator.serviceWorker.register('serviceWorker.js');
+    console.log("Service Worker Registered");
+  } catch (error) {
+    console.log("Service Worker Registration Failed");
+  }
+}
+
+const listToCache = [
+  './',
+  './index.html',
+  './manifest.json',
+  './favicon.ico',
+  './images/baseline_search_black_48dp.png'
+]
+
+self.addEventListener('install', async event=>{
+  const cache = await caches.open('static-cache');
+  cache.addAll(listToCache);
+});
+
+// self.addEventListener('activate', pEvent => {
+//   console.log('서비스워커 동작 시작됨');
+// });
+
+// self.addEventListener('fetch', pEvent => {
+//       pEvent.respondWith(
+//           caches.match(pEvent.request)
+//           .then(response => {
+//               if(!response){
+//                   console.log("네트워크로 데이터 요청!", pEvent.request)
+//                   return fetch(pEvent.request)
+//               }
+//               console.log("캐시에서 데이터 요청!", pEvent.request)
+//               return response;
+//           }).catch(err => console.log(err))
+//       );
+//   });
